89801277fe2f67d2dc796abe29006fd4125a0aa0
Scott Abbey <scott@scottabbey.com>
2021-04-16 14:17:48 -0400

    add stream parsing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import * as Fastify from 'fastify';
+import toString from 'stream-to-string';
 import discordPlugin from './plugins/discord';
 import handleUpdate from './routes/plex-update';
 
@@ -10,14 +11,14 @@ const server = Fastify.fastify({ logger: { level: 'info' } });
 server.route(handleUpdate);
 server.register(discordPlugin);
 
-server.addContentTypeParser(
-  'multipart/form-data',
-  { parseAs: 'string' },
-  function (req, body, done) {
-    const json = JSON.parse(body as string);
-    done(null, json);
-  }
-);
+server.addContentTypeParser('multipart/form-data', async function (
+  req: Fastify.FastifyRequest,
+  payload: Fastify.RawRequestDefaultExpression
+) {
+  const body = await toString(payload);
+  const json = JSON.parse(body);
+  return json;
+} as Fastify.FastifyContentTypeParser);
 
 server.listen(PORT, '0.0.0.0', (err, address) => {
   if (err) {
