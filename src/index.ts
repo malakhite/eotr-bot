8e833470063f2cc0d3018886bcff0d201fffc29f
Scott Abbey <scott@scottabbey.com>
2021-04-16 13:43:50 -0400

    Fix multipart/form-data to parse as json

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ const server = Fastify.fastify({ logger: { level: 'info' } });
 server.route(handleUpdate);
 server.register(discordPlugin);
 
+server.addContentTypeParser(
+  'multipart/form-data',
+  { parseAs: 'string' },
+  function (req, body, done) {
+    const json = JSON.parse(body as string);
+    done(null, json);
+  }
+);
+
 server.listen(PORT, '0.0.0.0', (err, address) => {
   if (err) {
     server.log.error(err);
