4e32e94240477f3a3f6eca164c46c80201ae7374
Scott Abbey <scott@scottabbey.com>
2021-04-16 18:13:20 -0400

    Add pre-validation hook to see what's going on

--- src/routes/plex-update/index.ts
+++ src/routes/plex-update/index.ts
@@ -3,47 +3,49 @@ import { Static, Type } from '@sinclair/typebox';
 import fp from 'fastify-plugin';
 
 const Message = Type.Object({
-  event: Type.String(),
-  user: Type.Boolean(),
-  owner: Type.Boolean(),
-  Account: Type.Object({
-    id: Type.Number(),
-    thumb: Type.Optional(Type.String({ format: 'uri' })),
-    title: Type.String(),
-  }),
-  Server: Type.Object({
-    title: Type.String(),
-    uuid: Type.String({ format: 'uuid' }),
-  }),
-  Player: Type.Object({
-    local: Type.Boolean(),
-    publicAddress: Type.String({ format: 'ipv4' }),
-    title: Type.String(),
-    uuid: Type.String({ format: 'uuid' }),
-  }),
-  Metadata: Type.Object({
-    librarySectionType: Type.String(),
-    ratingKey: Type.String(),
-    key: Type.String(),
-    grandparentRatingKey: Type.String(),
-    guid: Type.String(),
-    librarySectionId: Type.Number(),
-    type: Type.String(),
-    title: Type.String(),
-    grandparentKey: Type.String(),
-    parentKey: Type.String(),
-    grandparentTitle: Type.String(),
-    parentTitle: Type.String(),
-    summary: Type.String(),
-    index: Type.Number(),
-    parentIndex: Type.Number(),
-    thumb: Type.String(),
-    art: Type.String(),
-    parentThumb: Type.String(),
-    grandparentThumb: Type.String(),
-    grandparentArt: Type.String(),
-    addedAt: Type.Number(),
-    updatedAt: Type.Number(),
+  payload: Type.Object({
+    event: Type.String(),
+    user: Type.Boolean(),
+    owner: Type.Boolean(),
+    Account: Type.Object({
+      id: Type.Number(),
+      thumb: Type.Optional(Type.String({ format: 'uri' })),
+      title: Type.String(),
+    }),
+    Server: Type.Object({
+      title: Type.String(),
+      uuid: Type.String({ format: 'uuid' }),
+    }),
+    Player: Type.Object({
+      local: Type.Boolean(),
+      publicAddress: Type.String({ format: 'ipv4' }),
+      title: Type.String(),
+      uuid: Type.String({ format: 'uuid' }),
+    }),
+    Metadata: Type.Object({
+      librarySectionType: Type.String(),
+      ratingKey: Type.String(),
+      key: Type.String(),
+      grandparentRatingKey: Type.String(),
+      guid: Type.String(),
+      librarySectionId: Type.Number(),
+      type: Type.String(),
+      title: Type.String(),
+      grandparentKey: Type.String(),
+      parentKey: Type.String(),
+      grandparentTitle: Type.String(),
+      parentTitle: Type.String(),
+      summary: Type.String(),
+      index: Type.Number(),
+      parentIndex: Type.Number(),
+      thumb: Type.String(),
+      art: Type.String(),
+      parentThumb: Type.String(),
+      grandparentThumb: Type.String(),
+      grandparentArt: Type.String(),
+      addedAt: Type.Number(),
+      updatedAt: Type.Number(),
+    }),
   }),
 });
 export type MessageType = Static<typeof Message>;
@@ -68,14 +70,17 @@ const { DISCORD_PLEX_CHANNEL } = process.env;
 // export default handleUpdate;
 
 async function handleUpdate(fastify: FastifyInstance) {
-  fastify.post<{ Body: MessageType }>(
-    '/updates',
-    { schema: { body: Message } },
-    async (request, response) => {
+  fastify.route<{ Body: MessageType }>({
+    method: 'POST',
+    url: '/updates',
+    schema: { body: Message },
+    preValidation: async (request, reply) => {
+      console.log(request.body);
+    },
+    handler: async (request, response) => {
       const { body } = request;
-      console.log(body);
-    }
-  );
+    },
+  });
 }
 
 export default fp(handleUpdate);
