70219862b875da8319b0dbb834e61f34704c79fb
Scott Abbey <scott@scottabbey.com>
2024-03-09 06:52:04 -0500

    adjust date

--- src/constants.ts
+++ src/constants.ts
@@ -5,10 +5,10 @@ export const SONGWHIP_URL = 'https://songwhip.com/';
 export const WORDGAMES = [
 	{
 		game: 'Wordle',
-		startDate: dayjs('2021-06-19').startOf('day'),
+		startDate: dayjs('2021-06-18').startOf('day'),
 	},
 	{
 		game: 'Connections',
-		startDate: dayjs('2023-06-11').startOf('day'),
+		startDate: dayjs('2023-06-12').startOf('day'),
 	},
 ];
