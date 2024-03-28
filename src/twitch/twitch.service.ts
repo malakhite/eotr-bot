5a07f68ae8d3c31b5bd490298d659a08b28623f8
Scott Abbey <scott@scottabbey.com>
2024-03-27 20:38:22 -0400

    change to arrow function

--- src/twitch/twitch.service.ts
+++ src/twitch/twitch.service.ts
@@ -1,10 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectEventSubHttpListener } from '@nestjs-twurple/eventsub-http';
-import {
-	EventSubStreamOnlineEvent,
-	EventSubSubscription,
-} from '@twurple/eventsub-base';
+import { EventSubSubscription } from '@twurple/eventsub-base';
 import { EventSubMiddleware } from '@twurple/eventsub-http';
 import { Client, EmbedBuilder } from 'discord.js';
 
@@ -22,49 +19,6 @@ export class TwitchService implements OnModuleInit {
 		private readonly eventSubListener: EventSubMiddleware,
 	) {}
 
-	private async channelLiveHandler(event: EventSubStreamOnlineEvent) {
-		try {
-			this.logger.debug({ message: 'Received Twitch event', event });
-
-			const updatesChannel = this.discordClient.channels.cache.get(
-				this.configService.get('DISCORD_UPDATES_CHANNEL'),
-			);
-			if (!updatesChannel) {
-				this.logger.error({
-					message: 'Unable to open DISCORD_UPDATES_CHANNEL',
-					channel: this.configService.get('DISCORD_UPDATES_CHANNEL'),
-				});
-				return;
-			}
-			if (!updatesChannel.isTextBased()) {
-				this.logger.error({
-					message: 'DISORD_UPDATES_CHANNEL is not a text channel.',
-					channel: this.configService.get('DISCORD_UPDATES_CHANNEL'),
-				});
-				return;
-			}
-
-			const broadcaster = await event.getBroadcaster();
-			const stream = await event.getStream();
-
-			const channelUrl = new URL(broadcaster.name, 'https://twitch.tv');
-			const thumbnail = broadcaster.profilePictureUrl;
-
-			const goingLiveEmbed = new EmbedBuilder()
-				.setTitle(`${event.broadcasterDisplayName} is streaming now!`)
-				.setURL(channelUrl.toString())
-				.setThumbnail(thumbnail);
-
-			if (stream?.gameName) {
-				goingLiveEmbed.addFields({ name: 'Playing', value: stream.gameName });
-			}
-
-			await updatesChannel.send({ embeds: [goingLiveEmbed] });
-		} catch (e) {
-			this.logger.error(e);
-		}
-	}
-
 	async onModuleInit() {
 		await this.eventSubListener.markAsReady();
 
@@ -73,7 +27,47 @@ export class TwitchService implements OnModuleInit {
 		for await (const userId of TWITCH_USER_IDS) {
 			const eventSubscription = this.eventSubListener.onStreamOnline(
 				userId,
-				this.channelLiveHandler,
+				async (event) => {
+					this.logger.debug({ message: 'Received Twitch event', event });
+
+					const updatesChannel = this.discordClient.channels.cache.get(
+						this.configService.get('DISCORD_UPDATES_CHANNEL'),
+					);
+					if (!updatesChannel) {
+						this.logger.error({
+							message: 'Unable to open DISCORD_UPDATES_CHANNEL',
+							channel: this.configService.get('DISCORD_UPDATES_CHANNEL'),
+						});
+						return;
+					}
+					if (!updatesChannel.isTextBased()) {
+						this.logger.error({
+							message: 'DISORD_UPDATES_CHANNEL is not a text channel.',
+							channel: this.configService.get('DISCORD_UPDATES_CHANNEL'),
+						});
+						return;
+					}
+
+					const broadcaster = await event.getBroadcaster();
+					const stream = await event.getStream();
+
+					const channelUrl = new URL(broadcaster.name, 'https://twitch.tv');
+					const thumbnail = broadcaster.profilePictureUrl;
+
+					const goingLiveEmbed = new EmbedBuilder()
+						.setTitle(`${event.broadcasterDisplayName} is streaming now!`)
+						.setURL(channelUrl.toString())
+						.setThumbnail(thumbnail);
+
+					if (stream?.gameName) {
+						goingLiveEmbed.addFields({
+							name: 'Playing',
+							value: stream.gameName,
+						});
+					}
+
+					await updatesChannel.send({ embeds: [goingLiveEmbed] });
+				},
 			);
 			const testUrl = await eventSubscription.getCliTestCommand();
 			this.logger.log({
