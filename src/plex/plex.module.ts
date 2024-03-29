bfe10d7720d852117c4b5e11b0423811b6e5debe
Scott Abbey <scott@scottabbey.com>
2024-03-29 19:30:24 -0400

    fix exports/imports

--- src/plex/plex.module.ts
+++ src/plex/plex.module.ts
@@ -3,11 +3,8 @@ import { Module } from '@nestjs/common';
 import { PlexController } from './plex.controller';
 import { PlexService } from './plex.service';
 
-import { FilesService } from '../files/files.service';
-
 @Module({
 	providers: [PlexService],
 	controllers: [PlexController],
-	imports: [FilesService],
 })
 export class PlexModule {}
