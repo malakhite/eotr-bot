7b2512c06ad56c5162c4a420d0ae735876a0c3a0
Scott Abbey <scott@scottabbey.com>
2024-03-30 15:19:43 -0400

    remove file handling from plex endpoint

--- src/plex/plex.controller.ts
+++ src/plex/plex.controller.ts
@@ -1,11 +1,4 @@
-import {
-	Body,
-	Controller,
-	Post,
-	UploadedFile,
-	UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { PlexUpdateDto } from './plex-update.dto';
 import { PlexService } from './plex.service';
@@ -15,19 +8,12 @@ export class PlexController {
 	constructor(private readonly plexService: PlexService) {}
 
 	@Post('webhook')
-	@UseInterceptors(FileInterceptor('thumb'))
 	async updates(
 		@Body()
 		plexUpdateDto: PlexUpdateDto,
-
-		@UploadedFile()
-		thumb?: Express.Multer.File,
 	) {
 		if (plexUpdateDto.event === 'library.new') {
-			const result = await this.plexService.handleLibraryNew(
-				plexUpdateDto,
-				thumb,
-			);
+			const result = await this.plexService.handleLibraryNew(plexUpdateDto);
 
 			return result;
 		}
