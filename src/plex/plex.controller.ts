f171854e8c131164be86d0811d28930bd4cf8f27
Scott Abbey <scott@scottabbey.com>
2024-04-17 20:48:22 -0400

    add file handler

--- src/plex/plex.controller.ts
+++ src/plex/plex.controller.ts
@@ -1,14 +1,29 @@
-import { Body, Controller, Post, Query } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Logger,
+	Post,
+	Query,
+	UploadedFile,
+	UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 import { PlexUpdateDto } from './plex-update.dto';
 import { PlexService } from './plex.service';
 
 @Controller('plex')
 export class PlexController {
+	private readonly logger = new Logger(PlexController.name);
+
 	constructor(private readonly plexService: PlexService) {}
 
 	@Post('webhook')
+	@UseInterceptors(FileInterceptor('thumb'))
 	async updates(
+		@UploadedFile()
+		_thumb: Express.Multer.File,
+
 		@Query('secret')
 		secret: string,
 
